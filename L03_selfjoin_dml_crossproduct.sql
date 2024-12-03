b15abc2f33628ee2604189d5f31d20fff98bd581
Prabhath Buddhika <buddhikarap@gmail.com>
2024-12-03 22:20:23 +0530

    Update L03_selfjoin_dml_crossproduct.sql

--- L03_selfjoin_dml_crossproduct.sql
+++ L03_selfjoin_dml_crossproduct.sql
@@ -1,5 +1,6 @@
 -- using the cross product alone.
 -- cross product associates the tuples from one relation with each tuples from another.
 -- this allows creating one broad tuple that incorporates all attributes needed to evaluate
--- the required predicate. 
+-- the required predicate.
+-- using the rename operator 'as' is required but using the 'as' keyword is not compulsory.
 select B.supervisor from emp_super as A, emp_super as B where A.supervisor = B.person and A.person = 'Bob'; 
