e8a1b97fd33a572829424ba8c5858d8a04f8a11a
Prabhath Buddhika <buddhikarap@gmail.com>
2024-12-03 22:29:50 +0530

    Using Cross Product
    This SQL script shows the use of cross product to solve the self join problem as the problem name implies.

--- L03_selfjoin_dml_crossproduct.sql
+++ L03_selfjoin_dml_crossproduct.sql
@@ -2,5 +2,5 @@
 -- cross product associates the tuples from one relation with each tuples from another.
 -- this allows creating one broad tuple that incorporates all attributes needed to evaluate
 -- the required predicate.
--- using the rename operator 'as' is required but using the 'as' keyword is not compulsory.
+-- using the rename operator 'as' is required but using the 'as' keyword is not compulsory. 
 select B.supervisor from emp_super as A, emp_super as B where A.supervisor = B.person and A.person = 'Bob'; 
